d88734d7f30deff2884eb747466506a41ec222b8
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2026-06-13 01:45:24 -0300

    refactor: melhora legibilidade da validação

--- ex3.js
+++ ex3.js
@@ -2,12 +2,12 @@ import printConsoleScreen from "./printConsoleScreen.js";
 let goAgain = "s";
 while (goAgain === "s") {
     let num;
-    let fatorial = (n) => (n <= 0)? 1: n*fatorial(n-1);
+    const fatorial = (n) => (n <= 0)? 1: n*fatorial(n-1);
     do {
         num = parseInt(prompt("Insira um número inteiro positivo maior que 0: "));
     } while (num <= 0 || isNaN(num));
     printConsoleScreen(`Fatorial de ${num}: ${fatorial(num)}`);
     do {
-        goAgain = prompt("Deseja calcular o fatorial de outro número (S/N)?: ");
-    } while (goAgain.toLowerCase() !== "s" && goAgain.toLowerCase() !== "n");
+        goAgain = prompt("Deseja calcular o fatorial de outro número (S/N)?: ").toLowerCase();
+    } while (goAgain !== "s" && goAgain !== "n");
 }
