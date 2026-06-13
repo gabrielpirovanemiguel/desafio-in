5d1acd35a2fd3af19a38588d61c8fcff6a53a44a
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2026-06-13 13:56:29 -0300

    fix: adiciona o aviso de entrada inválida

--- ex3.js
+++ ex3.js
@@ -1,10 +1,11 @@
 import printConsoleScreen from "./printConsoleScreen.js";
 let goAgain = "s";
 while (goAgain === "s") {
-    let num;
     const fatorial = (n) => (n <= 0)? 1: n*fatorial(n-1);
+    let num;
     do {
         num = parseInt(prompt("Insira um número inteiro positivo maior que 0: "));
+        if (num <= 0 || isNaN(num)) printConsoleScreen('Entrada inválida, tente novamente.');
     } while (num <= 0 || isNaN(num));
     printConsoleScreen(`Fatorial de ${num}: ${fatorial(num)}`);
     do {
