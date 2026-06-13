1d32e3dddadc14758258814aac84bd59ed049d89
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2026-06-13 01:31:38 -0300

    fix: correção no cálculo das raízes

--- ex2.js
+++ ex2.js
@@ -3,14 +3,14 @@ let a;
 do {
     a = parseFloat(prompt("Insira uma raiz A ≠ 0 para a equação: "));
 } while (a === 0)
-let b = parseFloat(prompt("Insira a raiz B para a equação: "));
-let c = parseFloat(prompt("Insira a raiz C para a equação: "));
-let discriminant = (b**2) - (4*a*c);
+const b = parseFloat(prompt("Insira a raiz B para a equação: "));
+const c = parseFloat(prompt("Insira a raiz C para a equação: "));
+const discriminant = (b**2) - (4*a*c);
 if (discriminant > 0) {
-    let valueDiscriminant = discriminant ** 0.5;
-    let denominator = 2*a;
-    let x1 = -b + valueDiscriminant / denominator;
-    let x2 = -b - valueDiscriminant / denominator;
+    const valueDiscriminant = discriminant ** 0.5;
+    const denominator = 2*a;
+    const x1 = (-b + valueDiscriminant) / denominator;
+    const x2 = (-b - valueDiscriminant) / denominator;
     printConsoleScreen(`Essa equação possui duas raízes.\nValor de x1: ${x1.toFixed(2)}\nValor de x2: ${x2.toFixed(2)}`);
 } else if (discriminant === 0) {
     let x = -b / (2*a);
